cd214d5bd497041851e617d51281cb1be1bf706e
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-06 15:43:58 -0400

    commit

---
+++ panda_filtering.py
@@ -0,0 +1,24 @@
+#Filtering pandas DataFrames
+import pandas as pd 
+import numpy as np
+from io import StringIO
+
+brics = pd.read_csv("brics.csv", index_col=0)
+#to make sure it prints correctly
+print(brics)
+
+###We want the country with over 8 million km^2 ###
+
+# 1. select the area column
+brics["area"]
+#or 
+brics.loc[:, "area"]
+#or
+brics.iloc[:, 2]
+
+# 2. compare the area column
+is_huge = brics[:, 2] > 8
+
+# 3. Use the result to select the countries
+print(brics[is_huge])
+
