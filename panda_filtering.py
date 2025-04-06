499a0ccb6f4fce22f6f393a5e568829896cf976c
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-06 15:47:13 -0400

    Update panda_filtering.py

--- panda_filtering.py
+++ panda_filtering.py
@@ -12,13 +12,15 @@ print(brics)
 # 1. select the area column
 brics["area"]
 #or 
-brics.loc[:, "area"]
+#brics.loc[:, "area"]
 #or
-brics.iloc[:, 2]
+#brics.iloc[:, 2]
 
 # 2. compare the area column
-is_huge = brics[:, 2] > 8
+is_huge = brics["area"] > 8 
 
 # 3. Use the result to select the countries
 print(brics[is_huge])
 
+# 4. do it in one line
+print(brics[brics.loc[:, "area"]>8])
