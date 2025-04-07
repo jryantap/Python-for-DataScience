a9c3583ca802c1c27fdc528b6f25923b80267ab5
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-07 17:53:08 -0400

    Create ForWhileLoopsReview.py

---
+++ ForWhileLoopsReview.py
@@ -0,0 +1,27 @@
+#Loop over list of lists example 
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+#Write a for loop that goes through each sublist of house and prints out the x is y sqm,
+#where x is the name of the room and y is the area of the room.
+
+# Build a for loop from scratch
+for h in house:
+    name_of_room = h[0]
+    name_of_area = h[1]
+    print("the " + name_of_room + " is " + str(name_of_area) + " in sqm")
+
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0:
+      offset = offset-1
+    else : 
+      offset = offset+1     
+    print(offset)
