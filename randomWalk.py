2aaa199d860a621897a88c4797bed4c5675629e7
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-09 18:44:12 -0400

    Update randomWalk.py

--- randomWalk.py
+++ randomWalk.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 np.random.seed(123)
 #set the list
 tails = [0]
@@ -7,6 +6,6 @@ tails = [0]
 for x in range(10) : # do it 10 times
     coin = np.random.randint(0,2) # head or tails
     tails.append(tails[x] + coin) #set the element and show if it's head or tails
-
-print(tails)
+    
+print (tails)
 
