c26e4d714ddb7948d1ffeb26ec7791f2dabf65cc
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-09 18:56:33 -0400

    Update randomWalk.py

--- randomWalk.py
+++ randomWalk.py
@@ -8,3 +8,29 @@ for x in range(10) : # do it 10 times
     tails.append(tails[x] + coin) #set the element and show if it's head or tails
     
 print (tails)
+
+#===============================
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the 
+for x in range(100) :
+    # Set step: last element in random_walk
+    step = random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
