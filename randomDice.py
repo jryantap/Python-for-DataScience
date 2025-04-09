9989167841c712b756493ad36c1e0b11d571ea72
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-09 18:01:49 -0400

    Update randomDice.py

--- randomDice.py
+++ randomDice.py
@@ -8,17 +8,16 @@ np.random.seed(123)
 r = np.random.rand()
 print(r)
 
-#dice roll
-import numpy as np
-np.random.seed(123)
-
+#======dice roll======#
 # Starting step
 step = 50
 
 # Roll the dice
 dice = np.random.randint(1,7) 
 
-# Finish the control construct
+# if dice is 1 or 2, go one step down
+# if dice is 3, 4 or 5, go one step up
+# else, throw the dice again, the number of dice is the number of steps you go up. 
 if dice <= 2 :
     step = step - 1
 elif dice <=5 :
@@ -29,3 +28,4 @@ else :
 # Print out dice and step
 print(dice)
 print(step)
+
