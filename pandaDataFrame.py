b744d5f70e1a7ce9a474852c06901270c2a4afe0
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-14 16:52:07 -0400

    Update pandaDataFrame.py

--- pandaDataFrame.py
+++ pandaDataFrame.py
@@ -32,14 +32,14 @@ print (cars)
 ##### loc and iloc ####
 ##### I mostly used loc ####
 
-# import pandas as pd
-# cars = pd.read_csv('cars.csv', index_col = 0)
-# print(cars)
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+print(cars)
 
-## Print out drives_right column as Series ##
-# print(cars.loc[:, 'drives_right'])
-# # Print out drives_right column as DataFrame
-# print(cars.loc[:, 'drives_right'])
+# Print out drives_right column as Series ##
+print(cars.loc[:, 'drives_right'])
+# Print out drives_right column as DataFrame
+print(cars.loc[:, 'drives_right'])
 
-## Print out cars_per_cap and drives_right as DataFrame ##
-# print(cars.loc[:, ['cars_per_cap','drives_right']])
+# Print out cars_per_cap and drives_right as DataFrame ##
+print(cars.loc[:, ['cars_per_cap','drives_right']])
