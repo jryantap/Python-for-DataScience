4a529b8336121cd7f14f841f25539ff63db42d25
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-14 16:52:44 -0400

    Update ForWhileLoopsReview.py

--- ForWhileLoopsReview.py
+++ ForWhileLoopsReview.py
@@ -73,14 +73,14 @@ for lab, row in cars.iterrows() :
     print(lab + ": " + str(row['cars_per_cap']))
 
 #==============================
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
-# Code for loop that adds COUNTRY column
-for lab, row in cars.iterrows():
-    cars.loc[lab, "COUNTRY"] = row["country"].upper()
-# Print cars
-print(cars)
+# # Import cars data
+# import pandas as pd
+# cars = pd.read_csv('cars.csv', index_col = 0)
+# # Code for loop that adds COUNTRY column
+# for lab, row in cars.iterrows():
+#     cars.loc[lab, "COUNTRY"] = row["country"].upper()
+# # Print cars
+# print(cars)
 
 #==============================
 # Import cars data
@@ -90,6 +90,7 @@ cars = pd.read_csv('cars.csv', index_col = 0)
 # Use .apply(str.upper)
 for lab, row in cars.iterrows() :
     cars.loc[lab, "COUNTRY"] = row["country"].upper()
+    
 
 cars["COUNTRY"] = cars["country"].apply(str.upper)
 print(cars)
