7803b9733af077a90ef61ce8755e0f02229af997
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-04-14 18:52:57 -0400

    Update netflixProject.py

--- netflixProject.py
+++ netflixProject.py
@@ -4,8 +4,8 @@
 # You have been supplied with the dataset netflix_data.csv, along with the following table detailing the column names and descriptions. Feel free to experiment further after submitting!
 
 # Perform exploratory data analysis on the netflix_data.csv data to understand more about movies from the 1990s decade.
-# What was the most frequent movie duration in the 1990s? Save an approximate answer as an integer called duration (use 1990 as the decade's start year).
-# A movie is considered short if it is less than 90 minutes. Count the number of short action movies released in the 1990s and save this integer as short_movie_count.
+# 1. What was the most frequent movie duration in the 1990s? Save an approximate answer as an integer called duration (use 1990 as the decade's start year).
+# 2. A movie is considered short if it is less than 90 minutes. Count the number of short action movies released in the 1990s and save this integer as short_movie_count.
 
 import pandas as pd
 import matplotlib.pyplot as plt
