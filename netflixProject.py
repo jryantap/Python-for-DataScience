1bffa0551d32f66c6f9c650e2a947576d787f3b6
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2025-05-07 07:13:00 -0400

    Update netflixProject.py

--- netflixProject.py
+++ netflixProject.py
@@ -10,30 +10,30 @@
 # 1. What was the most frequent movie duration in the 1990s? Save an approximate answer as an integer called duration (use 1990 as the decade's start year).
 # 2. A movie is considered short if it is less than 90 minutes. Count the number of short action movies released in the 1990s and save this integer as short_movie_count.
 
-# import pandas as pd
-# import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# #read the file
-# netflix_df = pd.read_csv("netflix_data.csv")
+#read the file
+netflix_df = pd.read_csv("netflix_data.csv")
 
-# # ##test print first##
-# # print(netflix_df) #print ok
+# ##test print first##
+# print(netflix_df) #print ok
 
-# ### 1. What was the most frequent movie duration in the 1990s? Save an approximate answer as an integer called duration (use 1990 as the decade's start year).
-# # get 1990s column
-# # release_year = netflix_df[(netflix_df["release_year"] > 1990) & (netflix_df["release_year"] < 2000)]
-
-
+### 1. What was the most frequent movie duration in the 1990s? Save an approximate answer as an integer called duration (use 1990 as the decade's start year).
+# get 1990s column
 # release_year = netflix_df[(netflix_df["release_year"] > 1990) & (netflix_df["release_year"] < 2000)]
-# print(release_year)
 
 
-# #visualize the distribution, set how wide the bars are
-# plt.hist(release_year["duration"], bins=50, edgecolor='black')
-# plt.title("Distribution of Movie Durations in the 1990s")
-# plt.xlabel("Duration(in minutes)")
-# plt.ylabel("Frequency")
-# plt.show()
+release_year = netflix_df[(netflix_df["release_year"] > 1990) & (netflix_df["release_year"] < 2000)]
+print(release_year)
+
+
+#visualize the distribution, set how wide the bars are
+plt.hist(release_year["duration"], bins=50, edgecolor='black')
+plt.title("Distribution of Movie Durations in the 1990s")
+plt.xlabel("Duration(in minutes)")
+plt.ylabel("Frequency")
+plt.show()
 
 # #=====================================
 # # 2. A movie is considered short if it is less than 90 minutes. Count the number of short action movies released in the 1990s and save this integer as short_movie_count.
