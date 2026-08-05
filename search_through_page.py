98c5393799f073163c0794849bd06855c962e8d1
Jherico-Ryan Tapang <jherico.tapang@outlook.com>
2026-08-05 07:06:16 -0400

    commit
    continuing

--- search_through_page.py
+++ search_through_page.py
@@ -1,4 +1,4 @@
-# script that will search through website keyword. 
+# script that will search through a ServiceNow page for specific keywords
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,9 +7,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 # set keywords here for now, hard coded.
 KEYWORDS = ["MyChart", "Anesthesia", "missing CI", "Cisco AnyConnect"]
 
-# Plans
+# Plan:
 # get the website page
 # search all visible table rows on that current page
+
+# get the website page 
+driver = webdriver.Chrome()
+driver.get("http://example.com") #should be the direct link for the ServiceNow page
+
+# must login to the dashboard with user's credentials then get prompted 
+# this is the prompt 
+input ("Login to the dashboard and press Enter once done." )
+
+# create a list to store matches
+matches = []
 
 
 
