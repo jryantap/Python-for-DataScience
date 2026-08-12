8659f54d3c8ada8a3d469d551b6f38fe5284c582
Jherico-Ryan Tapang <jhericot@gmail.com>
2026-08-12 07:19:05 -0400

    Update search_through_page.py

--- search_through_page.py
+++ search_through_page.py
@@ -62,5 +62,8 @@ for row in rows:  #go through each row that selenium found
                               """, row)
 
 #print the matches found
+print (f"\nFound {len(matches)} maching rows:\n") # I think this is right, have to double check the notation.
 
-        
+## maybe non f-string.
+## print("\nFound " + str(len(matches)) + "matching rows: \n")
+
